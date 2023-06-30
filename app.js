1690a0098ca0898c952f14bb703191a6429b943a
Luis Arturo <luisdemaryori@gmail.com>
2023-06-30 02:33:02 -0500

    mejora002

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const keyClave = document.querySelector(".keyClave");
 class HashTable {
   //definir el valor constante
   constante = Math.random();
+  //se crea un constructor donde se va a almacenar todos los valores que se ingresen por teclado
   constructor() {
     this.buckets = {};
   }
@@ -48,12 +49,12 @@ const myHashTable = new HashTable();
 // Manejar el envío del formulario
 hashForm.addEventListener("submit", function (event) {
   event.preventDefault(); // Evitar que se recargue la página al presionar el boton de insertar
-
+  //se lee los valores introducidos por teclado
   const claveTeclado = keyInput.value;
   const valorTeclado = valueInput.value;
 
   myHashTable.insert(claveTeclado, valorTeclado);
-
+  //se limpiar el imput de la clave y del valor
   keyInput.value = "";
   valueInput.value = "";
   //mostar lo guardado en la tabla
@@ -63,11 +64,12 @@ hashForm.addEventListener("submit", function (event) {
 // funcion para mostrar la salida actual de la tabla hash
 function showOutput() {
   llenarDatos.innerHTML = "";
-
+  // se recorre el objeto buckets para luego colocar los valores obtenidos en la tabla
   for (let key in myHashTable.buckets) {
     const { valorIntroducido, clave } = myHashTable.buckets[key];
+    //se crea un elemento tr
     const fila = document.createElement("tr");
-
+    //luego se inserta los valores a la tabla
     fila.innerHTML = `
     <td>${key}</td>
     <td>${clave}</td>
@@ -82,12 +84,14 @@ function showOutput() {
 // Manejar el buscador del formulario
 formularioBuscar.addEventListener("submit", function (event) {
   event.preventDefault(); // Evitar que se recargue la página
-
+  //se lee el valor de la clave por teclado
   const claveTeclado = keyClave.value;
+  // se llama a los datos que involucran a esa clave
   const { valores, indiceObtenido } = myHashTable.get(claveTeclado);
   const { valorIntroducido, clave } = valores;
+  //se limpia el input de la clave
   keyClave.value = "";
-
+  //se crea un nuevo elemento tr y se agrega el valor buscado a la tabla
   const fila = document.createElement("tr");
   fila.innerHTML += `
                       <td>${indiceObtenido}</td>
